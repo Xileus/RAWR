68657110456232b6126a7b518625bbe294d6bcbf
Xileus <zachariah.minniecon@my.jcu.edu.au>
2015-03-09 11:09:45 +1000

    RAWR, I'm definitely not a dinosaur

--- src/WordCounter/App.java
+++ src/WordCounter/App.java
@@ -5,7 +5,26 @@ package WordCounter;
  */
 public class App {
     public static void main(String[] args) {
-        String text = TextReader.load("simplr.txt");
-        System.out.println(text);
+        System.out.println("Manual unit test of WordCounter\n" +
+                "\"simplr.txt\" has 5 \"words\"");
+        int tally = CountWords("simplr");
+        System.out.println("Actual number of \"words\" " + tally);
+
+        System.out.println("Manual unit test of WordCounter\n" +
+                "\"muliline.txt\" has 12 \"words\"");
+        int tally2 = CountWords("muliline");
+        System.out.println("Actual number of \"words\" " + tally2);
+    }
+    private static int CountWords(String string)
+
+    {
+        String text = TextReader.load(string +".txt");
+        int tally = 1;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == ' ' | text.charAt(i) == '\n') {
+                tally++;
+            }
+        }
+        return tally;
     }
 }
